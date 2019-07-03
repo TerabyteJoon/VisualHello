7f098c93c64e2da64fd747995459f83ce846f47b
Young Joon Park <joonpyj@hotmail.com>
2019-07-03 15:07:38 +0900

    Added two more lines

--- GitTutorial/Program.cs
+++ GitTutorial/Program.cs
@@ -13,6 +13,8 @@ namespace GitTutorial
             Console.WriteLine("Hello World");
             Console.WriteLine("World is mine");
             Console.WriteLine("Luna is mine");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("World is mine");
         }
     }
 }
